1b1ac5b082e34eea45626b057bdab546db4960cf
kyle harrington <kyleduganharrington@gmail.com>
2022-09-30 11:35:22 -0700

    fixing a whole lotta broken

--- microk8sInstall.sh
+++ microk8sInstall.sh
@@ -82,5 +82,49 @@ sleep 10
 # creates storage class and persistent volume claim for CSI driver
 echo creating storace class and PVC for CSI drivers....
 sleep 1
-sudo microk8s kubectl apply -f /yamls/storage/microk8s-nfs.yaml
+sudo microk8s kubectl apply -f /dt-microk8s-setup/yaml/storage/microk8s-nfs.yaml
 sleep 5
+
+# Deploy Dynatrace components. 
+# These files are local to this repo and have been modified to fit the specific needs for microk8s
+echo creating dynatrace namespace... 
+sleep 1
+sudo microk8s kubectl create namespace dynatrace 
+sleep 1
+echo the dynatrace csi drivers and operator pods will now be deployed. 
+sleep 1
+echo this script will pause between deployments as resources are created.
+sleep 1
+echo once all resources have been deployed, this script will issue a command to watch the pods in the dyantrace namespace start in your terminal 
+sleep 1
+echo please wait until all pods are running before exiting this script as follows:
+sleep 1
+echo webhook x2
+echo operator 
+echo csi drivers 3/3
+sleep 2
+echo ^ a total of 4 pods
+sleep 1 
+echo note that the CSI driver sometimes enters a 2/3 state
+echo deleting the pod will correct this
+sleep 2
+echo once all pods are running for 2 minutes, you can exit this script and deploy your dynakube.yaml
+sleep 2
+echo deploying dynatrace operator and sleeping for 60 seconds... 
+sleep 1 
+sudo microk8s kubectl apply -f /dt-microk8s-setup/yaml/operator/v1_19_operator.yaml
+sleep 60 
+echo deploying dynatrace csi driver pods and sleeping for 60 seconds... 
+sudo microk8s kubectl apply -f /dt-microk8s-setup/yaml/csi-driver/csi-driver_v1_19.yaml
+sleep 60
+echo running kubectl get pod -n dynatrace -w
+sleep 1 
+echo please wait until all pods are running before exiting this script
+sleep 1
+echo if the csi pod enters a 2/3 state exit this script and delete the pod to recycle it
+sleep 1
+echo once all pods are running deploy your dynakube.yaml
+sleep 1
+echo godspeed
+sleep 1
+sudo microk8s kubectl get pod -n dynatrace -w
